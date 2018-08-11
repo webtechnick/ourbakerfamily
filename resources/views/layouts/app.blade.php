1041be01663205dfe06feff79ac3d3eba9262e1d
Nick Baker <nick@webtechnick.com>
2018-08-11 17:48:18 -0600

    Webpackable and updated header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Our Baker Family
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -34,7 +34,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="/#rsvp">RSVP</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/#registry">Gift Registry</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/#gallery">Gallery</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
