4352f0bd1e9b107d98c73013eea58e3706e947f4
Nick Baker <nick@webtechnick.com>
2018-09-01 17:00:36 -0600

    Adding registry information

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,11 @@
                 <div class="card">
                     <div class="card-header">Registry</div>
                     <div class="card-body">
-                        Nick and Mandi are not currently registered. The registry information will be here once they are registered.
+                        <p>The most important thing is having you with us on our special day.  No gifts are needed or expected, however, we have been asked what we need or would like.  Our reply is, to build memories.</p>
+
+                        <p>In lieu of a honeymoon, we have decided to make memories as a family.  This December, we will be taking Skyler and Dean to DisneyWorld and Universal Orlando.  If you would like to contribute to our experience, <a href="https://paypal.me/WebTechNick" target="_blank">please donate to our Wishing Well!</a></p>
+
+                        <p>If you’re interested in a more traditional gift, we are also registered at <a href="https://www.amazon.com/wedding/share/tildeathdouspart" target="_blank">Amazon</a> and <a href="https://www.bedbathandbeyond.com/store/gift-registry-search?_requestid=736396#searchKeywordByName=546421921" target="_blank">Bed Bath & Beyond.</a></p>
                     </div>
                 </div>
             </div>
